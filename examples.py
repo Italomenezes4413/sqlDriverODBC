2e71627dab35d59f5b3fdfc9797c993878ed3180
Italomenezes4413 <74031259+Italomenezes4413@users.noreply.github.com>
2026-06-16 16:21:27 -0300

    TEstes
    tes

--- examples.py
+++ examples.py
@@ -1,25 +1,113 @@
 {
-    "workbench.colorTheme": "Dracula Theme",
+
     "workbench.colorCustomizations": {
+
         "editor.background": "#000000",
+
         "sideBar.background": "#000000",
+
         "activityBar.background": "#000000",
+
         "terminal.background": "#000000",
+
         "statusBar.background": "#000000",
+
         "titleBar.activeBackground": "#000000"
-    },
-    "python.createEnvironment.trigger": "off",
-    "security.workspace.trust.untrustedFiles": "open",
-    "explorer.confirmDelete": false,
-    "github.copilot.enable": {
-        "python": false,
-        "*": false
-    },
-    "explorer.confirmDragAndDrop": false,
-    "github.copilot.advanced": {
 
     },
+
+    "python.createEnvironment.trigger": "off",
+
+    "security.workspace.trust.untrustedFiles": "open",
+
+    "explorer.confirmDelete": false,
+
+    "github.copilot.enable": {
+
+        "python": false,
+
+        "*": false
+
+    },
+
+    "explorer.confirmDragAndDrop": false,
+
+    "github.copilot.advanced": {
+
+
+
+    },
+
     "editor.fontLigatures": true,
+
     "editor.fontFamily": "Fira code",
-    "editor.fontWeight":"500" 
+
+    "editor.fontWeight": "500",
+
+    "workbench.iconTheme": "material-icon-theme",
+
+    "git.openRepositoryInParentFolders": "never",
+
+    "editor.acceptSuggestionOnCommitCharacter": false,
+
+    "workbench.colorTheme": "Dracula Theme",
+
+    "terminal.integrated.profiles.windows": {
+
+        "PowerShell": {
+
+            "source": "PowerShell",
+
+            "icon": "terminal-powershell",
+
+            "path": "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe"
+
+        },
+
+        "Command Prompt": {
+
+            "path": "C:\\Windows\\System32\\cmd.exe",
+
+            "args": [],
+
+            "icon": "terminal-cmd"
+
+        },
+
+        "Git Bash": {
+
+            "path": "C:\\Program Files\\Git\\bin\\bash.exe",
+
+            "args": [
+
+                "--login",
+
+                "-i"
+
+            ]
+
+        },
+
+        "Ubuntu (WSL)": {
+
+            "path": "C:\\Windows\\System32\\wsl.exe",
+
+            "args": [
+
+                "-d",
+
+                "Ubuntu"
+
+            ]
+
+        }
+
+    },
+
+    "terminal.integrated.defaultProfile.windows": "Git Bash",
+
+    "terminal.integrated.copyOnSelection": false,
+
+    "explorer.confirmPasteNative": false
+
 }
